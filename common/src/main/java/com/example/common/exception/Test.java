f118d377a88149fc5468ba65e20ce4d24161b994
JiaWei Lee <gsdukbh@gmail.com>
2021-08-28 18:11:57 +0800

    init

---
+++ common/src/main/java/com/example/common/exception/Test.java
@@ -0,0 +1,9 @@
+package com.example.common.exception;
+
+/**
+ * created on  2021/8/28
+ *
+ * @author leejiawei
+ */
+public class Test {
+}
---
+++ common/src/main/java/com/example/common/log/Log.java
@@ -0,0 +1,9 @@
+package com.example.common.log;
+
+/**
+ * created on  2021/8/28
+ *
+ * @author leejiawei
+ */
+public class Log {
+}
