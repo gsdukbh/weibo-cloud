de346cffea0c2242fe136943ca5134fc34f2e1fd
JiaWei Lee <gsdukbh@gmail.com>
2021-08-30 22:26:26 +0800

    init

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,17 @@ public class GatewayApplication {
     public String h()
     {
         return "hello";
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Add a simple re-route from: /get to: http://httpbin.org:80
+                // Add a simple "Hello:World" HTTP Header
+                .route(p -> p
+                        .path("/get") // intercept calls to the /get path
+                        .filters(f -> f.addRequestHeader("Hello", "World")) // add header
+                        .uri("http://httpbin.org:80")) // forward to httpbin
+                .build();
     }
 }
